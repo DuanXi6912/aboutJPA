a42ba0818650c0602432f65a32c20cdb80511272
DuanXi6912 <120545609+DuanXi6912@users.noreply.github.com>
2023-06-21 16:20:06 +0700

    Update AboutJpaApplication.java

--- src/main/java/api/example/aboutJPA/AboutJpaApplication.java
+++ src/main/java/api/example/aboutJPA/AboutJpaApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AboutJpaApplication {
 
-	public static void Case(){
-		
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(AboutJpaApplication.class, args);
 	}
